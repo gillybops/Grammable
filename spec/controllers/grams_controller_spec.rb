7bf52b017ad1ba50bb79c384fe7d1a5a20ff871f
gillybops <gillybops@optonline.net>
2018-08-13 17:05:48 +0000

    Added a Detail Page for Grams

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,6 +1,20 @@
 require 'rails_helper'
 
+
 RSpec.describe GramsController, type: :controller do
+  describe "grams#show action" do
+    it "should successfully show the page if the gram is found" do
+      gram = FactoryBot.create(:gram)
+      get :show, params: { id: gram.id }
+      expect(response).to have_http_status(:success)
+    end
+
+    it "should return a 404 error if the gram is not found" do
+      get :show, params: { id: 'TACOCAT' }
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
   describe "grams#index action" do
     it "should successfully show the page" do
       get :index  
@@ -14,7 +28,6 @@ RSpec.describe GramsController, type: :controller do
       get :new
       expect(response).to redirect_to new_user_session_path
     end
-
 
     it "should successfully show the new form" do
       user = FactoryBot.create(:user)
@@ -33,11 +46,9 @@ RSpec.describe GramsController, type: :controller do
       expect(response).to redirect_to new_user_session_path
     end
 
- 
     it "should successfully create a new gram in our database" do
       user = FactoryBot.create(:user)
       sign_in user
-
 
       post :create, params: { gram: { message: "Hello!" } }
       expect(response).to redirect_to root_path
@@ -50,7 +61,6 @@ RSpec.describe GramsController, type: :controller do
     it "should properly deal with validation errors" do
       user = FactoryBot.create(:user)
       sign_in user
-
 
       gram_count = Gram.count
       post :create, params: { gram: { message: '' } }
